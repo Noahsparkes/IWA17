cbe6b659ba7015d567fb78a93c26fdf64cb89c3c
Noah19-cell <103570425+Noah19-cell@users.noreply.github.com>
2023-05-06 18:09:55 +0200

    commented explanation for most of code.

--- Challenge 1/file.js
+++ Challenge 1/file.js
@@ -20,7 +20,7 @@ const getDaysInMonth = (date) => new Date(date.getFullYear(), date.getMonth() + 
 // Only edit below 
 
 const createArray = (length) => {
-    const result = []
+    const result = []   
 
     for (let i = 0; i < length; i++ ){
         result.push(i); // returns array with numbers from 0 to lenght-1
@@ -31,6 +31,10 @@ const createArray = (length) => {
     console.log(result);
 }*/ 
 
+
+
+/*following creates an array of 5 objects, each reprisenting a week and containing 
+an array of 7 objects, each representing a day with its day of the week and day of the month.*/ 
 getDaysInMonth = (date) => {
   const year = date.getFullYear();
   const month = date.getMonth() + 1;
@@ -69,9 +73,9 @@ const createData = () => {
 
     return weeks;
 }
-/*above code snippet creates an array of 5 objects, each reprisenting a week and containing 
-an array of 7 objects, each representing a day with its day of the week and day of the month.*/ 
 
+
+//The purpose of this function(addCell) is to add a new HTML table cell (<td>) to an existing table row.
 const addCell = (existing, classString, value) => {
     const result = /* html */ `
         <td ${classString}>
@@ -111,37 +115,35 @@ const addCell = (existing, classString, value) => {
 }
 */
 
+// Takes in an array of data representing weeks and days
 const createHtml = (data) => {
-    let result = '';
+    let result = ''; // Initialize an empty string called result
 
+    // Loop over each element in the data array
     for (const { week, days } of data) {
-        let inner = '';
+        let inner = ''; // Initialize an empty string called inner
+
+        // Add a table cell for the week number
         inner = addCell(inner, 'table__cell table__cell_sidebar', `Week ${week}`);
-    
+
+        // Loop over each day in the current week
         for (const [dayOfWeek, value] of Object.entries(days)) {
-            const isToday = new Date() === value;
-            const isWeekend = dayOfWeek == 1 || dayOfWeek == 7;
-            const isAlternate = week % 2 === 0;
+            // Determine whether the day is today's date, a weekend, or falls on an alternate week
+            // Set the classString variable based on these conditions
             let classString = 'table__cell';
+            if (new Date() === value) classString = `${classString} table__cell_today`;
+            if (dayOfWeek == 1 || dayOfWeek == 7) classString = `${classString} table__cell_weekend`;
+            if (week % 2 === 0) classString = `${classString} table__cell_alternate`;
 
-            if (isToday) {
-                classString = `${classString} table__cell_today`;
-            }
-
-            if (isWeekend) {
-                classString = `${classString} table__cell_weekend`;
-            }
-
-            if (isAlternate) {
-                classString = `${classString} table__cell_alternate`;
-            }
-
+            // Add a table cell for the current day
             inner = addCell(inner, classString, value);
         }
 
+        // Add the completed row to the result string
         result = `${result}<tr>${inner}</tr>`;
     }
 
+    // Return the completed HTML table
     return result;
 };
 
